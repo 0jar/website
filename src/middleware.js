c4eee41c0c2016f1437b5d2844b7549c7691c1de
Jarema <git@jarema.me>
2026-06-23 12:17:47 +0300

    cleanup now unused code

--- src/middleware.js
+++ src/middleware.js
@@ -1,9 +1,7 @@
-import { defineMiddleware, sequence } from "astro:middleware";
+import { defineMiddleware } from "astro:middleware";
 import { getLocaleFromUrl } from "@/i18n/routing";
 
-const locale = defineMiddleware(({ url, locals }, next) => {
+export const onRequest = defineMiddleware(({ url, locals }, next) => {
   locals.lang = getLocaleFromUrl(url);
   return next();
 });
-
-export const onRequest = sequence(locale);
