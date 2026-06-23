3a3d912cdf316793efedcf01ce1e3102e96532fd
Jarema <git@jarema.me>
2026-06-23 16:28:38 +0300

    screw CSV lol

---
+++ src/content/guestbook/guestbook.ts
@@ -0,0 +1,115 @@
+export const messages = [
+  {
+    name: "Kisei",
+    website: "https://tuansondesigns.com",
+    social: "Discord for now",
+    location: "HN, Vietnam",
+    referrer: "https://uses.tech/",
+    created_at: "2026-05-17T07:39:00.000Z",
+    message: `Hello Jarema!
+This website looks crazy great it made me so envious lmao haha. It felt a small fireplace in the midst of chaotic storm, alluring you with lullaby and cats to rest. I love how accessible it is and I'm also on a whole site revision to redesign my website to be more accessible and more "personal"
+
+Cheers!`
+  },
+  {
+    name: "Brennan",
+    website: "https://brennan.day",
+    social: "https://social.lol/@brennan",
+    location: "Canada",
+    referrer: "We're webring neighbours!",
+    created_at: "2026-05-14T06:00:21.000Z",
+    message: `Hello Jarema! So nice to meet you! You are right next to me on the a11y webring, and I absolutely LOVE your site. I think we have a lot in common in terms of design thinking :) cheers!`
+  },
+  {
+    name: "Will",
+    website: "",
+    social: "",
+    location: "",
+    referrer: "",
+    created_at: "2026-04-17T13:49:10.000Z",
+    message: `Hi Jarema,
+This website looks crazy good and the design is awesome. Visiting this website really feels personal and really brings out the personality that you put into the website. I rate this a 100/10. It feels like I'm visiting someone's house and they show me their personal *private* room (lol).
+
+Also love when my cursor turns into a cat whenever I click onto this website. And the whole aesthetic, really Windows XP like.`
+  },
+  {
+    name: "Tayro",
+    website: "",
+    social: "",
+    location: "",
+    referrer: "Pressed random website on the indieweb site",
+    created_at: "2026-03-30T08:57:01.000Z",
+    message: `Hey Jarema :)
+
+I randomly stumbled across your blog surfing the web today and I gotta say the website looks **amazing**, this must have been some crazy work!
+
+So I was just reading your blog about the software you use in 2026 and you expressed having trouble switching away from google photos. Me personally I use ente photos and really like it maybe you can give that a try. It's e2ee, open source and since they know that migrating is a pain they have made it a very simple and easy process. I have been using ente for about a year now and I love it. It even has cross platform shared albums which is a huge plus for me. Their website is: [https://ente.io](https://ente.io)
+
+Also I tried contacting you over matrix and it didn't work so maybe check out if something is broken there
+
+Hope this helps have a nice day :D`
+  },
+  {
+    name: "Adim :d",
+    website: "https://jarema.me",
+    social: "",
+    location: "🦅🇵🇱🔥POLSKAAA 🔥🇵🇱🦅",
+    referrer: "Jarema discord profile connections :D",
+    created_at: "2026-03-03T12:02:14.000Z",
+    message: `hewwo`
+  },
+  {
+    name: "Grizz",
+    website: "https://grizz.atwebpages.com/",
+    social: "",
+    location: "USA",
+    referrer: "Hotline webring",
+    created_at: "2026-02-26T20:44:12.000Z",
+    message: `Hi Jarema, I had fun on site site. New cat, who dis? :clown:`
+  },
+  {
+    name: "Anonymous",
+    website: "",
+    social: "",
+    location: "",
+    referrer: "",
+    created_at: "2026-02-17T22:13:21.000Z",
+    message: `oops I sent it 3 times`
+  },
+  {
+    name: "Saintwupid",
+    website: "",
+    social: "Discord and Line",
+    location: "Thailand",
+    referrer: "through your discord",
+    created_at: "2026-02-17T22:12:52.000Z",
+    message: `kindest person in doghouse :3`
+  },
+  {
+    name: "~hedy",
+    website: "https://home.hedy.dev/",
+    social: "",
+    location: "",
+    referrer: "the meta ring :)",
+    created_at: "2025-12-27T06:26:27.000Z",
+    message: `hello. I love the design of your website and oh my god the cat pics sections is sooo cute and creative~ I love it!`
+  },
+  {
+    name: "fr0st",
+    website: "https://fr0st.xyz/",
+    social: "",
+    location: "",
+    referrer: "https://webring.bucketfish.me/",
+    created_at: "2025-12-24T16:22:09.000Z",
+    message: `*hey webring neighbor :P* **just wanted to say your site is really cool! ;)**`
+  },
+  {
+    name: "G",
+    website: "",
+    social: "",
+    location: "",
+    referrer: "",
+    created_at: "2025-12-21T07:22:54.000Z",
+    message: `bing bong, tuesday we free :3`
+  }
+];
